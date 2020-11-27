e029c74222214b66e64771af5f4880e378372d0f
ericlyeric <ericlyeric@gmail.com>
2020-11-26 22:34:10 -0800

    main page wip

--- client/src/components/HomePagePublic.js
+++ client/src/components/HomePagePublic.js
@@ -4,13 +4,13 @@ import { Jumbotron, Container } from 'react-bootstrap';
 const HomePagePublic = () => {
     return (
         <Jumbotron fluid>
-        <Container>
-            <h1>Scale Project</h1>
-            <p>
-                The application is used to track your weight daily, and chart the progress you've made. Good luck!
-            </p>
-        </Container>
-    </Jumbotron>
+            <Container>
+                <h1>Scale Project</h1>
+                <p>
+                    The application is used to track your weight daily, and chart the progress you've made. Good luck!
+                </p>
+            </Container>
+        </Jumbotron>
     )
 }
 
--- client/src/components/common/DashboardBar.js
+++ client/src/components/common/DashboardBar.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Nav, Button } from 'react-bootstrap';
 import { BarChart2, List } from 'react-feather';
 import '../../styles/Dashboard.css';
 
-const DashboardBar = () => {
+const DashboardBar = ({ active, setActive }) => {
     return(
-        <Nav id="sidebarMenu" className="col-md-2 d-md-block bg-light sidebar">
+        <div id="sidebarMenu" className="col-md-2 d-md-block bg-light sidebar">
         <div className="sidebar-sticky pt-2 nav flex-column">
           <Nav.Item>
-              <Button type="button" className="btn btn-light active" block>
+              <Button 
+                type="button" 
+                className={`btn btn-light ${active === 'progress' ? 'active' : null}`} 
+                onClick={() => setActive('progress')}
+                block
+              >
                 <BarChart2 size={18}/>
                 <span>Progress</span>
               </Button>
           </Nav.Item>
           <Nav.Item>
-              <Button type="button" className="btn btn-light" block>
+              <Button 
+                type="button" 
+                className={`btn btn-light ${active === 'history' ? 'active' : null}`} 
+                onClick={() => setActive('history')}
+                block
+              >
                 <List size={18}/>
                 <span>Weight History</span>
               </Button>
           </Nav.Item>
         </div>
-      </Nav>
+      </div>
     );
 }
 
