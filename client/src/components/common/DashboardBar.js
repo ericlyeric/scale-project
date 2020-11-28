b4529d40b8bd3e581fcf835f6a1b57b375936641
ericlyeric <ericlyeric@gmail.com>
2020-11-27 22:17:24 -0800

    add template for pages progress and history

--- client/src/components/common/DashboardBar.js
+++ client/src/components/common/DashboardBar.js
@@ -1,35 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Nav, Button } from 'react-bootstrap';
 import { BarChart2, List } from 'react-feather';
 import '../../styles/Dashboard.css';
 
 const DashboardBar = ({ active, setActive }) => {
     return(
-        <div id="sidebarMenu" className="col-md-2 d-md-block bg-light sidebar">
-        <div className="sidebar-sticky pt-2 nav flex-column">
-          <Nav.Item>
-              <Button 
-                type="button" 
-                className={`btn btn-light ${active === 'progress' ? 'active' : null}`} 
-                onClick={() => setActive('progress')}
-                block
-              >
-                <BarChart2 size={18}/>
-                <span>Progress</span>
-              </Button>
-          </Nav.Item>
-          <Nav.Item>
-              <Button 
-                type="button" 
-                className={`btn btn-light ${active === 'history' ? 'active' : null}`} 
-                onClick={() => setActive('history')}
-                block
-              >
-                <List size={18}/>
-                <span>Weight History</span>
-              </Button>
-          </Nav.Item>
-        </div>
+      <div className="sidebar-sticky pt-2 nav flex-column">
+        <Nav.Item>
+          <Button 
+            type="button" 
+            className={`btn btn-light ${active === 'progress' ? 'active' : ''}`} 
+            onClick={() => setActive('progress')}
+            block
+          >
+            <BarChart2 size={18}/>
+            <span>Progress</span>
+          </Button>
+        </Nav.Item>
+        <Nav.Item>
+          <Button 
+            type="button" 
+            className={`btn btn-light ${active === 'history' ? 'active' : ''}`} 
+            onClick={() => setActive('history')}
+            block
+          >
+            <List size={18}/>
+            <span>History</span>
+          </Button>
+        </Nav.Item>
       </div>
     );
 }
---
+++ client/src/components/common/TableView.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+const TableView = () => {
+    return (
+        <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1,001</td>
+                    <td>Lorem</td>
+                    <td>ipsum</td>
+                    <td>dolor</td>
+                    <td>sit</td>
+                  </tr>
+                  <tr>
+                    <td>1,002</td>
+                    <td>amet</td>
+                    <td>consectetur</td>
+                    <td>adipiscing</td>
+                    <td>elit</td>
+                  </tr>
+                </tbody>
+        </Table>
+    )
+}
+
+export default TableView;
